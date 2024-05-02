e9c3c8bf277554a13f3cb5c297f3e7c8dc0af9ea
Dhogan06 <68254682+Dhogan06@users.noreply.github.com>
2024-05-02 14:24:20 -0600

    Changes For Testing

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ import DauigiWebTools from "./webtools.js";
 let tools = new DauigiWebTools();
 tools.cookies.setCookie("TestCookie", "Testing");
 console.log(tools.cookies.getCookie("TestCookie"));
+
+window.tools = tools;
